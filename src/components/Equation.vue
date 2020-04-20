7337e66d19dea1ba744871dded34e8b8b5ee2f2e
Wutthipat ChalermChatwichien <wutthipat@ddproperty.com>
2020-04-21 05:14:46 +0700

    Create equation route

--- src/components/Equation.vue
+++ src/components/Equation.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
+  <div class='container'>
     <label for='inputEquation'>Please insert the array of equations</label>
-    <input id='inputEquation' type='text' v-model="equationStr" v-on:input='checkInputEmpty'>
-    <button type='button' v-on:click='sendRequest'>Click</button>
-    <br/>
-    <label>{{respStr}}</label>
+    <b-input class='mb-3' id='inputEquation' type='text' v-model="equationStr" v-on:input='checkInputEmpty' />
+    <b-button class='mb-3' type='button' v-on:click='sendRequest'>Click</b-button>
+    <div>
+      <label>{{respStr2}}</label>
+    </div>
   </div>
 </template>
 
@@ -12,10 +13,12 @@
 import axios from 'axios'
 export default {
   name: "equation",
+  components: {
+  },
   data: function() {
     return {
       equationStr: '',
-      respStr: ''
+      respStr2: ''
     }
   },
   methods: {
@@ -26,12 +29,30 @@ export default {
         })
         console.log(equations);
         const res = await axios.post('http://localhost:3000/api/equations', { equations })
-        if (!res.data) this.respStr = 'There is no solution for these eqautions'
-        else this.respStr = JSON.stringify(res.data)
+        if (!res.data) this.respStr2 = 'There is no solution for these eqautions'
+        else this.respStr2 = JSON.stringify(res.data)
     },
     checkInputEmpty() {
-      if (this.equationStr.length === 0) this.respStr = ''
+      if (this.equationStr.length === 0) this.respStr2 = ''
     }
+  },
+  mounted: function() {
+    if (localStorage.getItem('equationStr')) this.equationStr = JSON.parse(localStorage.getItem('equationStr'));
+    if (localStorage.getItem('respStr2')) this.respStr2 = JSON.parse(localStorage.getItem('respStr2'));
+  },
+  watch: {
+    equationStr: {
+      handler() {
+        localStorage.setItem('equationStr', JSON.stringify(this.equationStr));
+      },
+      deep: true,
+    },
+    respStr2: {
+      handler() {
+        localStorage.setItem('respStr2', JSON.stringify(this.respStr2));
+      },
+      deep: true,
+    },
   }
 }
 </script>
