39fe5759258aacf984555154251e5600bd099f11
Wutthipat ChalermChatwichien <wutthipat@ddproperty.com>
2020-04-21 05:13:46 +0700

    Create resume route and add necessary files and photos

---
+++ src/components/Resume.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class='container'>
+    <img :src='resumeImg' class="card-img-top">
+  </div>
+</template>
+
+<script>
+import resumeImg from '../assets/WutthipatCV.jpg'
+export default {
+  data: function(){
+    return {
+      resumeImg
+    }
+  }
+}
+</script>
