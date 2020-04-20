e0884940a2798ef1b492703b0b9740107cb6e8ac
Wutthipat ChalermChatwichien <wutthipat@ddproperty.com>
2020-04-21 05:11:07 +0700

    Setup project configurations and remove unused files

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,46 @@
 <template>
   <div id="app">
+    <myheader />
     <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/sequence">Sequence</router-link>
-      <router-link to="/equation">Equation</router-link>
-      <router-link to="/linebot">Linebot</router-link>
+      <b-container class='mb-5'>
+        <b-row align-h='center'>
+          <b-col sm='1' align-v='center'>
+            <router-link to="/">Home</router-link>
+          </b-col>
+          <b-col sm='1' align-v='center'>
+            <router-link to="/sequence">Sequence</router-link>
+          </b-col>
+            <b-col sm='1' align-v='center'>
+          <router-link to="/equation">Equation</router-link>
+          </b-col>
+          <b-col sm='1' align-v='center'>
+            <router-link to="/direction">Direction</router-link>
+          </b-col>
+          <b-col sm='1' align-v='center'>
+            <router-link to="/linebot">Linebot</router-link>
+          </b-col>
+          <b-col sm='1' align-v='center'>
+            <router-link to="/resume">Resume</router-link>
+          </b-col>
+        </b-row>
+      </b-container>
     </div>
     <router-view/>
+    <myfooter />
   </div>
 </template>
+
+<script>
+import header from './components/Header'
+import footer from './components/Footer'
+export default {
+  name: 'app',
+  components: {
+    myheader: header,
+    myfooter: footer,
+  }
+}
+</script>
 
 <style>
 #app {
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import VueRouter from 'vue-router'
 import App from './App'
-import Home from './components/HelloWorld'
+import Home from './components/Home'
 import Sequence from './components/Sequence'
 import Equation from './components/Equation'
+import Direction from './components/Direction'
 import Linebot from './components/Linebot'
+import Resume from './components/Resume'
 
 
 Vue.use(VueRouter)
@@ -13,7 +18,9 @@ const routes = [
   { path: '/', component: Home },
   { path: '/sequence', component: Sequence },
   { path: '/equation', component: Equation },
+  { path: '/direction', component: Direction },
   { path: '/linebot', component: Linebot },
+  { path: '/resume', component: Resume },
 ]
 
 const router = new VueRouter({ routes })
