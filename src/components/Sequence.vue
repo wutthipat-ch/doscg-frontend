9cd049afde340d4431ca697718d317b223664e1f
Wutthipat ChalermChatwichien <wutthipat@ddproperty.com>
2020-04-21 05:14:24 +0700

    Create sequence route

--- src/components/Sequence.vue
+++ src/components/Sequence.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
+  <div class='container'>
     <label for='inputSeq'>Please insert the sequence, comma delimeter</label>
-    <input id='inputSeq' type='text' v-model="sequenceStr" v-on:input='checkInputEmpty'>
-    <button type='button' v-on:click='sendRequest'>Click</button>
-    <br/>
-    <label>{{respStr}}</label>
+    <b-input class='mb-3' id='inputSeq' type='text' v-model="sequenceStr" v-on:input='checkInputEmpty' />
+    <b-button class='mb-3' type='button' v-on:click='sendRequest'>Click</b-button>
+    <div>
+      <label>{{respStr1}}</label>
+    </div>
   </div>
 </template>
 
@@ -15,19 +16,37 @@ export default {
   data: function() {
     return {
       sequenceStr: '',
-      respStr: ''
+      respStr1: ''
     }
   },
   methods: {
     async sendRequest() {
         const seq = this.sequenceStr
         const res = await axios.post('http://localhost:3000/api/sequences', { seq })
-        if (!res.data) this.respStr = 'There is no solution for this sequence'
-        else this.respStr = JSON.stringify(res.data)
+        if (!res.data) this.respStr1 = 'There is no solution for this sequence'
+        else this.respStr1 = JSON.stringify(res.data)
     },
     checkInputEmpty() {
-      if (this.sequenceStr.length === 0) this.respStr = ''
+      if (this.sequenceStr.length === 0) this.respStr1 = ''
     }
+  },
+  mounted: function() {
+    if (localStorage.getItem('sequenceStr')) this.sequenceStr = JSON.parse(localStorage.getItem('sequenceStr'));
+    if (localStorage.getItem('respStr1')) this.respStr1 = JSON.parse(localStorage.getItem('respStr1'));
+  },
+  watch: {
+    sequenceStr: {
+      handler() {
+        localStorage.setItem('sequenceStr', JSON.stringify(this.sequenceStr));
+      },
+      deep: true,
+    },
+    respStr1: {
+      handler() {
+        localStorage.setItem('respStr1', JSON.stringify(this.respStr1));
+      },
+      deep: true,
+    },
   }
 }
 </script>
