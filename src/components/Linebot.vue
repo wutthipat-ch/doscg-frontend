57a68bfd09794e9019f241733026726b3b127f19
Wutthipat ChalermChatwichien <wutthipat@ddproperty.com>
2020-04-21 05:15:18 +0700

    Create Line bot route

--- src/components/Linebot.vue
+++ src/components/Linebot.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
+  <div class='container'>
     <label for='inputMessage'>Please insert message to send to line bot</label>
-    <input id='inputMessage' type='text' v-model="message" v-on:input='checkInputEmpty'>
-    <button type='button' v-on:click='sendRequest'>Click</button>
-    <br/>
-    <label>{{respStr}}</label>
+    <b-input class='mb-3' id='inputMessage' type='text' v-model="message" v-on:input='checkInputEmpty' />
+    <b-button class='mb-3' type='button' v-on:click='sendRequest'>Click</b-button>
+    <div>
+      <label>{{respStr4}}</label>
+    </div>
   </div>
 </template>
 
@@ -15,19 +16,37 @@ export default {
   data: function() {
     return {
       message: '',
-      respStr: ''
+      respStr4: ''
     }
   },
   methods: {
     async sendRequest() {
         const message = this.message
         const res = await axios.post('http://localhost:3000/linebot/messages', { message })
-        if (res.status !== 200) this.respStr = 'Something went wrong, cannot send message to line bot'
-        else this.respStr = 'Send message successfully'
+        if (res.status !== 200) this.respStr4 = 'Something went wrong, cannot send message to line bot'
+        else this.respStr4 = 'Send message successfully'
     },
     checkInputEmpty() {
-      if (this.message.length === 0) this.respStr = ''
+      if (this.message.length === 0) this.respStr4 = ''
     }
+  },
+  mounted: function() {
+    if (localStorage.getItem('message')) this.message = JSON.parse(localStorage.getItem('message'));
+    if (localStorage.getItem('respStr4')) this.respStr4 = JSON.parse(localStorage.getItem('respStr4'));
+  },
+  watch: {
+    message: {
+      handler() {
+        localStorage.setItem('message', JSON.stringify(this.message));
+      },
+      deep: true,
+    },
+    respStr4: {
+      handler() {
+        localStorage.setItem('respStr4', JSON.stringify(this.respStr4));
+      },
+      deep: true,
+    },
   }
 }
 </script>
